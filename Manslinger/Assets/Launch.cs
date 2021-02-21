d6899dbf5ebc6d27d18c71ee1c7eca3366db8c9c
miva2 <miva2-_-@hotmail.com>
2021-02-21 23:03:45 +0100

    Added gifs + forgot to commit something

--- Manslinger/Assets/Launch.cs
+++ Manslinger/Assets/Launch.cs
@@ -6,13 +6,7 @@ using UnityEngine;
 public class Launch : MonoBehaviour
 {
     public GameObject projectile;
-    //public Rigidbody projectileRigidbody;
 
-    //public float explosionForce;
-    //public Vector3 explosionPosition;
-    //public float explosionRadius;
-
-    //public Vector3 launchForce;
     public Vector3 launchDirection;
     public float launchPower;
 
@@ -21,7 +15,7 @@ public class Launch : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //projectileRigidbody = GetComponentInChildren<Rigidbody>();
+        
     }
 
     // Update is called once per frame
@@ -30,14 +24,9 @@ public class Launch : MonoBehaviour
         if(Input.GetMouseButtonDown(0))
         {
             projectileRigidBody = Instantiate(projectile, transform.position, Quaternion.identity).transform.Find("Root/Hips").GetComponent<Rigidbody>(); // yeah I know this is not great
-            //projectileRigidbody.AddExplosionForce(explosionForce, explosionPosition, explosionRadius);
-            //projectileRigidbody.AddForce(launchForce); // LOL don't apply force on the rigidbody I give in public
+           
             projectileRigidBody.AddForce(launchDirection * launchPower);
         }
     }
 
-    //private Vector3 CalculateExplosionPosition()
-    //{
-    //    return transform.position - new Vector3(0, 0, 3);
-    //}
 }
