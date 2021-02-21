0bb93de3508ca2b9192f7713d652220270c33b8f
miva2 <miva2-_-@hotmail.com>
2021-02-21 22:53:40 +0100

    shoot ragdolls. Included some failed attempts in comments.

--- Manslinger/Assets/Launch.cs
+++ Manslinger/Assets/Launch.cs
@@ -6,11 +6,22 @@ using UnityEngine;
 public class Launch : MonoBehaviour
 {
     public GameObject projectile;
+    //public Rigidbody projectileRigidbody;
+
+    //public float explosionForce;
+    //public Vector3 explosionPosition;
+    //public float explosionRadius;
+
+    //public Vector3 launchForce;
+    public Vector3 launchDirection;
+    public float launchPower;
+
+    private Rigidbody projectileRigidBody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //projectileRigidbody = GetComponentInChildren<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -18,8 +29,15 @@ public class Launch : MonoBehaviour
     {
         if(Input.GetMouseButtonDown(0))
         {
-            Instantiate(projectile, transform.position, Quaternion.identity);
-
+            projectileRigidBody = Instantiate(projectile, transform.position, Quaternion.identity).transform.Find("Root/Hips").GetComponent<Rigidbody>(); // yeah I know this is not great
+            //projectileRigidbody.AddExplosionForce(explosionForce, explosionPosition, explosionRadius);
+            //projectileRigidbody.AddForce(launchForce); // LOL don't apply force on the rigidbody I give in public
+            projectileRigidBody.AddForce(launchDirection * launchPower);
         }
     }
+
+    //private Vector3 CalculateExplosionPosition()
+    //{
+    //    return transform.position - new Vector3(0, 0, 3);
+    //}
 }
